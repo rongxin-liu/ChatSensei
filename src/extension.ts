e3ec0ae398a604f1d88bfd72a60ca6bc7c010866
Rongxin Liu <rongxinliu.dev@gmail.com>
2023-03-28 10:42:28 +0800

    added skeleton codes

---
+++ src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log("chatSensei is now active!");
+}
+
+export function deactivate() {}
